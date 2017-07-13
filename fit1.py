c2bb8810f40ddc3590d8e8b265f4b912744cbd81
Unknown <chansigit@gmail.com>
2017-07-13 12:03:11 +0800

    code finished

---
+++ fit1.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import scipy
+import scipy.stats
+size = 30
+x = scipy.arange(size)
+y = scipy.int_(scipy.round_(scipy.stats.vonmises.rvs(5,size=size)*47))
+h = plt.hist(y, bins=range(48), color='w')
+
+dist_names = ['gamma', 'beta', 'rayleigh', 'norm', 'pareto']
+
+for dist_name in dist_names:
+    dist = getattr(scipy.stats, dist_name)
+    param = dist.fit(y)
+    pdf_fitted = dist.pdf(x, *param[:-2], loc=param[-2], scale=param[-1]) * size
+    plt.plot(pdf_fitted, label=dist_name)
+    plt.xlim(0,47)
+plt.legend(loc='upper right')
+plt.show()
