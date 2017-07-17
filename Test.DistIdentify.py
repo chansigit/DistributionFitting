3a8bb8b9c312499f8cc3886da605bc5ad7b743d4
chansigit <chansigit@gmail.com>
2017-07-17 17:04:23 +0800

    format transformation
    tab to space

--- Test.DistIdentify.py
+++ Test.DistIdentify.py
@@ -4,7 +4,7 @@ from DistIdentify.Identification import DistIdentify
 
 data = np.random.normal(loc=40, scale=3.57,size=500)
 matcher = DistIdentify(data=data)
-#matcher.summary( plot=True)
-#matcher.setCandidateDists([st.norm, st.lognorm, st.gamma, st.erlang, st.invweibull,st.invgauss, st.invgamma, st.beta])
+matcher.summary( plot=True)
+matcher.setCandidateDists([st.norm, st.lognorm, st.gamma, st.erlang, st.invweibull,st.invgauss, st.invgamma, st.beta])
 #matcher.fitting(plot=True, file="fitting.pdf")
 matcher.fitting(plot=True, file=None)
